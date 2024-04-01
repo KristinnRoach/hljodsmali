b33058b19393e7ba19306758ec9509b14f61d4a8
KristinnRoach <kristinnroach@gmail.com>
2024-04-01 19:26:55 +0000

    customise-a seinna

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          kid starter template - NextJS - SASS &nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
