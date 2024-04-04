3b73798addfa5986becc2a10aec274e3d5d3e393
KristinnRoach <kristinnroach@gmail.com>
2024-04-04 22:34:58 +0000

    cleanup f deploy

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,17 +4,7 @@ import { useState, useEffect } from 'react';
 
 import styles from './Nav.module.scss';
 
-// FÆRA POCKETBASE DÓT
-const pb = new PocketBase(process.env.DATABASE_URL);
-
 export default function Nav({ children }: { children: React.ReactNode }) {
-  const [pocketbase, setPocketbase] = useState(0);
-
-  useEffect(() => {
-    pb.authStore.loadFromCookie(document.cookie);
-    setPocketbase(pb);
-  }, []);
-
   return (
     <nav className={styles.nav}>
       <Link href="/">Home</Link>
