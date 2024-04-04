804a178f85c8f76aa680282634259c1759082d55
KristinnRoach <kristinnroach@gmail.com>
2024-04-04 22:29:20 +0000

    cleanup

--- src/app/(pages)/(auth)/auth-only-page/page.tsx
+++
@@ -1,7 +0,0 @@
-const session = null;
-
-export default function AuthOnlyPage() {
-  // if (!session) throw new Error('Auth is required to access this resource');
-
-  return <main>this is an auth-only page</main>;
-}
